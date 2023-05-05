ec0ed264b72e5b7a7f7c3b5cb07996fb472362b6
Chris <1080691521@u.icesi.edu.co>
2023-05-05 10:07:17 -0500

    Added attributes, constructor, getters and setters for Order Class. Updated readme with quality indicators for iteration 1.

--- README.md
+++ README.md
@@ -3,28 +3,42 @@
 This project is part of the solution for the integrative task of the algorithms
 and programming subject.
 
-### __Contributors__
+## __Contributors__
 
 - Cristian Eduardo Botina (A00395008)
 - Juan Manuel Marín (A00382037)
 - Óscar Andrés Gómez (A00394142)
 
-### Documentation
+## Documentation
 
 _*Project Requirements:*_ https://docs.google.com/document/d/1cTjnD_E07SSicd29bIpiBxFhH3Oek-mUToNbVr49PwY/edit
 
 _*Scenarios and Use Cases:*_ https://docs.google.com/document/d/1Rs_S2UvqD8iIWewG-Qz_qdFJUIlJRiw4/edit
 
-### Quality indicators
+## Quality indicators
+
+
+### Iteration 1
+
+__Commit:__ b385128
+
+__Failure density:__ 1/3 = 0.333
+
+__Reliability:__ = 1 - 1/3 = 0.667
+
+__Completeness:__ = 3/7 = 0.429
 
 ---
 
 ### __Iteration 2__
 
-__Errors density:__ 0.2
+__Commit:__ 458cc27
+
+__Failure density:__ 0.2
 
 __Reliability:__ 0.8
 
 __Completeness:__ 5 / 7
 
 ---
+
--- src/main/java/com/mercadolibre/integradora2/model/Order.java
+++ src/main/java/com/mercadolibre/integradora2/model/Order.java
@@ -1,4 +1,44 @@
 package com.mercadolibre.integradora2.model;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Order {
+    private String customerName;
+    private final ArrayList<Product> products;
+    private final LocalDate DATE;
+    private double totalPrice;
+
+    public Order(String customerName) {
+        this.customerName = customerName;
+        this.products = new ArrayList<>();
+        this.DATE = LocalDate.now();
+        this.totalPrice = 0;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+        totalPrice += product.getPrice();
+    }
+
+    public LocalDate getDATE() {
+        return DATE;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
 }
